61f3cada7aedda297030beb8e65be831c849645c
nkhs <fang.chp@gmail.com>
2018-03-31 09:45:04 +0800

    .

--- src/models/errormessage.js
+++ src/models/errormessage.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose"),
   Schema = mongoose.Schema;
 
 var errorMessageSchema = new Schema({
+  from: String,
   type: String,
   message: String,
   detail: String,
+  time: Date
 });
 
 mongoose.model("ErrorMessage", errorMessageSchema);
--- src/routes/errormessage.js
+++ src/routes/errormessage.js
@@ -7,7 +7,10 @@ var util = require("../lib/util");
 var ErrorMessage = mongoose.model("ErrorMessage");
 
 router.post("/insert", (req, res, next) => {
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     var newErrorMessage = new ErrorMessage(req.body.errormessage);
+    newErrorMessage.detail = `ip: ${ip}`;
+    newErrorMessage.time = new Date();
     newErrorMessage.save().then(() => {
         res.json({
             success: true
